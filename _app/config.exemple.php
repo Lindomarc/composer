f7d9b5a01234168b5e728145f79f642bcf15b20a
lindomarc <lindomarc@outlook.com>
2021-05-22 20:07:19 -0300

    feat: config file]

---
+++ _app/config.exemple.php
@@ -0,0 +1,8 @@
+<?php
+	const MAIL_MAILER = 'smtp';
+	const MAIL_HOST = 'smtp.mailtrap.io';
+	const MAIL_PORT = '2525';
+	const MAIL_USERNAME = 'XXXXXXXXXXXXXX';
+	const MAIL_PASSWORD = 'XXXXXXXXXXXXXX';
+	const MAIL_ENCRYPTION = 'tls';
+	
